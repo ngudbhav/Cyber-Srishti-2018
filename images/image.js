efaf07d785faf4294a948076a92d2dbc86e46f10
Udbhav Gambhir <ngudbhav05@hotmail.com>
2018-03-17 23:40:04 +0530

    Coming soon

--- images/image.js
+++ images/image.js
@@ -1,10 +1,10 @@
 var j = require("jimp");
-j.read("logo.png", function(err, lenna){
+j.read("test.jpg", function(err, lenna){
 console.log("executing");
 	if(err) throw err;
 	else{
-		lenna.resize(500,500)//720x1280//4x2.5
-		.write("logo1.png");
+		lenna.resize(413,536)//720x1280//4x2.5
+		.write("test.jpg");
 		console.log("done");	
 		}
 });
