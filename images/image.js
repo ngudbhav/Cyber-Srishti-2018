8d4d5da45e9a5ab496976bfa5fd94b210a542ce7
Udbhav Gambhir <ngudbhav05@hotmail.com>
2018-03-21 07:53:18 +0530

    WIP

--- images/image.js
+++ images/image.js
@@ -1,10 +1,10 @@
 var j = require("jimp");
-j.read("cyber srishti_logo3.png", function(err, lenna){
+j.read("gallery9.jpg", function(err, lenna){
 console.log("executing");
 	if(err) throw err;
 	else{
-		lenna.resize(400,400)//720x1280//4x2.5
-		.write("cyber srishti_logo3.png");
+		lenna.resize(200,200)//720x1280//4x2.5
+		.write("gallery9-mini.jpg");
 		console.log("done");	
 		}
 });
