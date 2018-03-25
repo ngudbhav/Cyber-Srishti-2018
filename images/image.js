407f2997c18b9a18736272d67aff538a785879e6
Udbhav Gambhir <ngudbhav05@hotmail.com>
2018-03-25 13:09:24 +0530

    Responsiveness Update

--- images/image.js
+++ images/image.js
@@ -1,10 +1,10 @@
 var j = require("jimp");
-j.read("cyber srishti_logo3.png", function(err, lenna){
+j.read("./sponsors/10.png", function(err, lenna){
 console.log("executing");
 	if(err) throw err;
 	else{
-		lenna.resize(600,400)//720x1280//4x2.5
-		.write("cyber srishti_logo3.png");
+		lenna.resize(150,150)//720x1280//4x2.5
+		.write("./sponsors/10.png");
 		console.log("done");	
 		}
 });
